ccd3d56b41312db43c2d733589a8215cc3d10bdd
souhail-ouabou <souhail7ouabou@gmail.com>
2023-10-15 04:07:40 +0100

    add provisioner examples

--- main.tf
+++ main.tf
@@ -9,10 +9,11 @@ variable "env_prefix" {}
 variable "my_ip" {}
 variable "instance_type" {}
 variable "public_key_location" {}
+variable "private_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cider_block
-  tags       = {
+  tags = {
     Name = "${var.env_prefix}-vpc"
   }
 }
@@ -21,14 +22,14 @@ resource "aws_subnet" "myapp-subnet-1" {
   vpc_id            = aws_vpc.myapp-vpc.id
   cidr_block        = var.subnet_cider_block
   availability_zone = var.avail_zone
-  tags              = {
+  tags = {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
   vpc_id = aws_vpc.myapp-vpc.id
-  tags   = {
+  tags = {
     Name : "${var.env_prefix}-igw"
   }
 }
@@ -49,27 +50,27 @@ resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = [
       var.my_ip
     ]
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"
     ]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = [
       "0.0.0.0/0"
     ]
@@ -103,7 +104,7 @@ resource "aws_instance" "myapp-server" {
   ami           = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
-  subnet_id              = aws_subnet.myapp-subnet-1.id
+  subnet_id = aws_subnet.myapp-subnet-1.id
   vpc_security_group_ids = [
     aws_default_security_group.default-sg.id
   ]
@@ -112,7 +113,27 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.ssh-key.key_name
 
-  user_data = file("entry-script.sh")
+  #user_data = file("entry-script.sh")
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_location)
+  }
+  provisioner "file" {
+    source      = "entry-script.sh"
+    destination = "/home/ec2-user/entry-script-on-ec2.sh"
+  }
+
+  provisioner "remote-exec" {
+    script = file("entry-script.sh")
+
+  }
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > output.txt"
+  }
+
+
   tags = {
     Name : "${var.env_prefix}-server"
   }
@@ -120,9 +141,9 @@ resource "aws_instance" "myapp-server" {
 }
 
 output "aws_ami_id" {
-value = data.aws_ami.latest-amazon-linux-image.id
+  value = data.aws_ami.latest-amazon-linux-image.id
 }
 
 output "aws_public_ip" {
-value = aws_instance.myapp-server.public_ip
+  value = aws_instance.myapp-server.public_ip
 }
