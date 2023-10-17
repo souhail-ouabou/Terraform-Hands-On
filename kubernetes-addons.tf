19810676f383699580eaa5cf089ddbbf2ae1be2e
souhail-ouabou <souhail7ouabou@gmail.com>
2023-10-17 02:49:25 +0100

    Fix this error: subdir modules/kubernetes-addons/helm-addon not found - Update the version

--- kubernetes-addons.tf
+++ kubernetes-addons.tf
@@ -1,5 +1,5 @@
 module "kubernetes_addons" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.25.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.32.1"
 
   eks_cluster_id = module.eks_blueprints.eks_cluster_id
 
@@ -7,4 +7,6 @@ module "kubernetes_addons" {
   enable_amazon_eks_aws_ebs_csi_driver = true
 
 }
+
+
 
