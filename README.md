a442ccf8977565433285e55dbaa7e3e2b304fb2c
souhail-ouabou <souhail7ouabou@gmail.com>
2023-10-17 02:17:46 +0100

    Create EKS cluster

--- README.md
+++ README.md
@@ -8,7 +8,15 @@
 
 ### executes the actions proposed in a terraform plan without confirmation
 
-    terraform apply -auto-approve: 
+    terraform apply -auto-approve
+
+### connect to kubernetes
+
+    aws eks update-kubeconfig --name myapp-eks-cluster --region eu-west-2
+
+### verify
+
+    kubectl get nodes
 
 ### apply configuration with variables
 
--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,31 @@
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
-variable vpc_cidr_block {}
-variable private_subnet_cidr_blocks {}
-variable public_subnet_cidr_blocks {}
+variable "vpc_cidr_block" {}
+variable "private_subnet_cidr_blocks" {}
+variable "public_subnet_cidr_blocks" {}
 
 data "aws_availability_zones" "available" {}
 
 
 module "myapp-vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.64.0"
+  version = "5.0.0"
 
-  name            = "myapp-vpc"
-  cidr            = var.vpc_cidr_block
+  name = "myapp-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs             = data.aws_availability_zones.available.names
   private_subnets = var.private_subnet_cidr_blocks
   public_subnets  = var.public_subnet_cidr_blocks
-  azs             = data.aws_availability_zones.available.names
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
   tags = {
     "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
   }
